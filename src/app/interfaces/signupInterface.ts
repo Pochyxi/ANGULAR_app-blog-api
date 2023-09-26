714ced2bb4828f772908adc50d4fb5e632b80149
Pochyxi <adienerlopez@gmail.com>
2023-09-26 15:25:38 +0200

    Implementazione della login - signup - logout Implementazione di Posts e Post per visualizzare tutti i post (paginazione da implementare)
    Implementato authService e PostService
    Implementato interceptor per il Bearer Token e il csrf token
    
    Implementate le rotte di navigazione home(/) login(/login) signup(/signup)

---
+++ src/app/interfaces/signupInterface.ts
@@ -0,0 +1,6 @@
+export interface SignupInterface {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  POSTSAPIURL: 'http://localhost:8080/api/posts',
-  LOGINAPIURL: 'http://localhost:8080/api/auth/login',
-  SIGNUPIAPIURL: 'http://localhost:8080/api/auth/signup',
+  ROOTAPIURL: 'http://localhost:8080/api',
+  POSTSAPIURL: '/posts',
+  LOGINAPIURL: '/auth/login',
+  SIGNUPAPIURL: '/auth/signup',
 };
